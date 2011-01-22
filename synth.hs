3dc7ebccfbeb00dc027fc2e53b6e89ced87c8f2d
Dan <orblivion@orblivion-laptop.(none)>
2011-01-22 12:19:08 -0600

    Now made SafeValue a type that represents what can be used in evaluating a signal. SignalValue is what gets passed between components, unsanitized. Sanitize converts a SignalValue into a SafeValue, though right now it's not particularly safe.

--- synth.hs
+++ synth.hs
@@ -3,27 +3,29 @@ import Sound.Pulse.Simple
 samplesPerSecond :: (Num a) => a
 samplesPerSecond = 44100
 
-type SigValue = Float
-data Signal = Signal [SigValue]
+type SafeValue = Float
+data SignalValue = SignalValue SafeValue
+data Signal = Signal [SignalValue]
 
-flatSignal val = Signal [val, val..]
+flatSignal val = toSignal [val, val..]
 
 takeSeconds s (Signal sigdata) = take (s * samplesPerSecond) sigdata
 
 a :: Float
 a = 1.0 / samplesPerSecond
 
+sanitize (SignalValue val) = val
+toSignal :: [SafeValue] -> Signal
+toSignal values = Signal $ map SignalValue values
+
 osc_sin :: Signal -> Signal -> Signal
-osc_sin frequencySig amplitudeSig = Signal [ampVal * (sin $ 2*pi*freqVal*(t/samplesPerSecond)) | (t, ampVal, freqVal) <- (zip3 [1..] ampVals freqVals)] where
-    Signal ampVals = amplitudeSig
-    Signal freqVals = frequencySig
+osc_sin frequencySig amplitudeSig = toSignal [ampVal * (sin $ 2*pi*freqVal*(t/samplesPerSecond)) | (t, ampVal, freqVal) <- (zip3 [1..] ampVals freqVals)] where
+    Signal ampSigVals = amplitudeSig
+    Signal freqSigVals = frequencySig
+    ampVals = map sanitize ampSigVals
+    freqVals = map sanitize freqSigVals
 
--- mySin = ([tapering t * (sin $ 2*pi*440*(t/rate))|t<-[1..rate*seconds]] :: [Float]) where
---     tapering t = rate / (rate + t)
---     rate = 44100
---     seconds = 2
-
-mySin = takeSeconds 2 $ osc_sin (Signal [220.0, 220.002 ..]) (Signal [1.0, 0.999988 ..] )
+mySin = map sanitize $ takeSeconds 2 $ osc_sin (toSignal [220.0, 220.002 ..]) (toSignal [1.0, 0.999988 ..] )
 
 main=do
     s<-simpleNew Nothing "example" Play Nothing "this is an example application"
