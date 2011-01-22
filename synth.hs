b3637cda27fc0b4cb833dd10b1461f5c4458cf31
Dan <orblivion@orblivion-laptop.(none)>
2011-01-22 10:54:16 -0600

    Basic sin wave works, can send it frequency and amplitude signals!

--- synth.hs
+++ synth.hs
@@ -1,19 +1,20 @@
 import Sound.Pulse.Simple
 
-samplingRate = 44100.0
+samplesPerSecond :: (Num a) => a
+samplesPerSecond = 44100
 
 type SigValue = Float
 data Signal = Signal [SigValue]
 
 flatSignal val = Signal [val, val..]
 
-takeSeconds s = take (s * samplingRate)
+takeSeconds s (Signal sigdata) = take (s * samplesPerSecond) sigdata
 
 a :: Float
-a = 1.0 / samplingRate
+a = 1.0 / samplesPerSecond
 
 osc_sin :: Signal -> Signal -> Signal
-osc_sin frequencySig amplitudeSig = Signal [ampVal * (sin $ 2*pi*freqVal*(t/samplingRate)) | t <-[1..], ampVal <- ampVals, freqVal <- freqVals ] where
+osc_sin frequencySig amplitudeSig = Signal [ampVal * (sin $ 2*pi*freqVal*(t/samplesPerSecond)) | (t, ampVal, freqVal) <- (zip3 [1..] ampVals freqVals)] where
     Signal ampVals = amplitudeSig
     Signal freqVals = frequencySig
 
@@ -22,7 +23,7 @@ osc_sin frequencySig amplitudeSig = Signal [ampVal * (sin $ 2*pi*freqVal*(t/samp
 --     rate = 44100
 --     seconds = 2
 
-mySin = takeSeconds 2 $ osc_sin flatSignal 440 flatSignal 1
+mySin = takeSeconds 2 $ osc_sin (Signal [220.0, 220.002 ..]) (Signal [1.0, 0.999988 ..] )
 
 main=do
     s<-simpleNew Nothing "example" Play Nothing "this is an example application"
