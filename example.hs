5919a1a93c2a7b81e51f031770f82c456aa6e2cc
Dan <orblivion@orblivion-laptop.(none)>
2011-01-22 09:03:15 -0600

    Sin wave now tapers over 2 seconds

--- example.hs
+++ example.hs
@@ -1,8 +1,14 @@
 import Sound.Pulse.Simple
 
+mySin = ([tapering t * (sin $ 2*pi*440*(t/rate))|t<-[1..rate*seconds]] :: [Float]) where
+    tapering t = rate / (rate + t)
+    rate = 44100
+    seconds = 2
+
+
 main=do
     s<-simpleNew Nothing "example" Play Nothing "this is an example application"
         (SampleSpec (F32 LittleEndian) 44100 1) Nothing Nothing
-    simpleWrite s ([sin $ 2*pi*440*(t/44100)|t<-[1..44100*10]] :: [Float])
+    simpleWrite s mySin
     simpleDrain s
     simpleFree s
