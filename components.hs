a06b4d5efd9625bddedecedcb59245a365d77c20
Dan <orblivion@orblivion-laptop.(none)>
2011-01-26 21:41:40 -0600

    Started on sawtooth. Doesn't particularly work.

--- components.hs
+++ components.hs
@@ -4,15 +4,51 @@ import Sound.Pulse.Simple
 import Signals
 import List
 
-osc_sin :: FrequencySignal -> AmplitudeSignal -> Signal
-osc_sin frequencySig amplitudeSig = toSignal [ampVal * (sin $ 2*pi*freqVal*(t/samplesPerSecond)) | (t, ampVal, freqVal) <- (zip3 [1..] ampVals freqVals)] where
+type BasicOscillator = FrequencySignal -> AmplitudeSignal -> Signal
+
+-- make types for all the different parts of the time equation so I don't get them messed up
+-- for instance, t will be of a type that has the domain -1 - 1. stuff like that
+-- look up Haskell Fractional too in case that helps
+
+-- osc_sawtooth :: BasicOscillator 
+-- osc_sawtooth frequencySig amplitudeSig = toSignal [ampVal * (sawfunc $ freqVal*(t/samplesPerSecond)) | (t, ampVal, freqVal) <- (zip3 timeloop ampVals freqVals)] where
+--     sawfunc time = time
+--     timeloop = [0 .. samplesPerSecond - 1] ++ timeloop
+--     ampVals = sanitize amplitudeSig
+--     freqVals = sanitize frequencySig
+
+osc_sawtooth :: BasicOscillator 
+osc_sawtooth frequencySig amplitudeSig = toSignal $ [ sawfunc t freqVal ampVal | (t, freqVal, ampVal) <- zip3 [0..] ampVals freqVals ] where
+    sawfunc :: Integer -> SafeValue -> SafeValue -> SafeValue
+    -- sawfunc t freqVal ampVal = -1 + ampVal * 2 * (fromIntegral $ mod (t*440) samplesPerSecond) / samplesPerSecond
+    sawfunc t freqVal ampVal = -1 + 2 * (fromIntegral $ mod (t*freqVal) samplesPerSecond) / samplesPerSecond
     ampVals = sanitize amplitudeSig
     freqVals = sanitize frequencySig
+
+-- t = [-1 + (2*t)/samplesPerSecond] + val
+    
+
+
+-- osc_triangle :: BasicOscillator 
+-- osc_triangle frequencySig amplitudeSig = Signal osc_triangle_part frequencySig amplitudeSig 0 where
+--     osc_triangle_part frequencySig amplitudeSig t | t < halfway = ampVal * ( 
+
+
+osc_sine :: BasicOscillator 
+osc_sine frequencySig amplitudeSig = toSignal [ampVal * (sin $ 2*pi*freqVal*(t/samplesPerSecond)) | (t, ampVal, freqVal) <- (zip3 [1..] ampVals freqVals)] where
+    ampVals = sanitize amplitudeSig
+    freqVals = sanitize frequencySig
+
+
+
 
 sig_adder :: [Signal] -> Signal
 sig_adder insignals = toSignal outvalues where
     invalues = map fromSignal insignals
     outvalues = map sum $ transpose invalues
+
+
+
 
 play :: SoundSignal -> IO ()
 play signal = do
--- main.hs
+++ main.hs
@@ -1,17 +1,14 @@
 import Components
 import Signals 
 
-
 amp_lfo = sig_adder [(flatSignal 0.75), raw_lfo] where
-    raw_lfo = osc_sin (specialize $ flatSignal 2 ) (specialize $ flatSignal 0.5 )
+    raw_lfo = osc_sine (specialize $ flatSignal 2 ) (specialize $ flatSignal 0.5 )
 
 freq_lfo = sig_adder [(flatSignal 440), raw_lfo] where
-    raw_lfo = osc_sin (specialize $ flatSignal 5 ) (specialize $ flatSignal 50 )
+    raw_lfo = osc_sine (specialize $ flatSignal 5 ) (specialize $ flatSignal 50 )
 
-main_sin = osc_sin (specialize freq_lfo ) (specialize amp_lfo  )
+main_sin = osc_sawtooth (specialize freq_lfo ) (specialize amp_lfo  )
 
 mySin = takeSeconds 2 main_sin
-
-
 
 main = play $ specialize mySin
