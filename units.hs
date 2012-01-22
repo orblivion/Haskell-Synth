ad1388987738bdf5f215dbc4633bd76f601c6706
Daniel Krol <orblivion@gmail.com>
2012-01-21 18:45:12 -0600

    Rearrange code more logically, add some extra things I didn't have before.

--- units.hs
+++ units.hs
@@ -1,3 +1,4 @@
+-- The nature of units
 class Unit u where
     __ :: Float -> u
 
@@ -9,51 +10,74 @@ class Range r where
     __dummy2 :: r -> r
     __dummy2 x = x
 
-data SecondDomain   = SecondDomain
-instance Domain SecondDomain
-
-data CycleDomain    = CycleDomain
-instance Domain CycleDomain
-
-data SampleDomain   = SampleDomain
-instance Domain SampleDomain
 
 
-data Amplitude = Amplitude
-instance Range Amplitude
-
+-- The nature of domains, ranges, and conversions.
 
 data Progression domain where
     Progression :: (Domain domain) =>  Float -> Progression domain 
 
+data Conversion domain perDomain where
+    Conversion :: (Domain domain, Domain perDomain) => Float -> Conversion domain perDomain
+
 instance (Domain domain) => Unit (Progression domain) where
     __ n = Progression n
 
+--instance (Range range) => Unit (Range range) where
+--    __ n = Progression n
+
 instance (Domain domain, Domain perDomain) => Unit (Conversion domain perDomain) where
     __ n = Conversion n
+
+
+
+
+-- Things that domains, ranges, and conversions can do
 
 (+:) :: Progression domain -> Progression domain -> Progression domain
 (+:) (Progression a) (Progression b) = Progression (a + b)
 
 
-data Conversion domain perDomain where
-    Conversion :: (Domain domain, Domain perDomain) => Float -> Conversion domain perDomain
-
+-- It has been pointed out to me that this, as it stands, offers me no type safety.
 (*:) :: Progression domain_a -> Conversion domain_b domain_a -> Progression domain_b
 (*:) (Progression a) (Conversion f) = Progression (a * f)
 
+
+
+
+-- Some specific domains and ranges
+
+data SecondDomain   = SecondDomain
+instance Domain SecondDomain
 type Second = Progression SecondDomain
 second :: Float -> Second
 second a = Progression a
 
+data CycleDomain    = CycleDomain
+instance Domain CycleDomain
+type Cycle = Progression CycleDomain
+cycle :: Float -> Cycle 
+cycle a = Progression a
+
+data SampleDomain   = SampleDomain
+instance Domain SampleDomain
 type Sample = Progression SampleDomain
-type Cycle  = Progression CycleDomain
+sample :: Float -> Sample
+sample a = Progression a
 
 type Frequency = Conversion CycleDomain SecondDomain
 frequency :: Float -> Frequency
 frequency a = Conversion a
 
-type SamplingRate = Conversion Sample Second
+type SamplingRate = Conversion SampleDomain SecondDomain
+samplingrate :: Float -> SamplingRate 
+samplingrate a = Conversion a
+
+
+--data AmplitudeRange = AmplitudeRange
+--instance Range Amplitude
+
+
 
 a :: Cycle
 a =  second 5 +: __ 5  *: __ 20
