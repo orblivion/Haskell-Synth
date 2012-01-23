d63d8310579ed4fe51338027b47e4bcffb1bddc9
Daniel Krol <orblivion@gmail.com>
2012-01-22 23:07:13 -0600

    Adds a bunch of notes to the beginning of units.hs describing how the type system should work.

--- units.hs
+++ units.hs
@@ -1,3 +1,23 @@
+-- Here's the overall plan:
+
+-- UnitValue is the type of everything.
+-- "Domain" and "Range" go away. Instead we get the Unit, which is a typeclass that signifies something as a type of unit. Defines __ :: (Unit u) UnitValue u a -> a
+-- UnitValue (Unit u) => u Float/SafeValue
+
+-- Progression is a sub-typeclass of Unit, which implements (+) on those Units. We can have various functions, then, that operate on Progressions, since they'd be used as a marker of how far along a list of items.
+-- Similarly, SignalType can specify what can come out of a component. Be it a frequency, or whatnot. Or maybe we don't want to play favorites with what can go into what... hmm. Maybe for InputTypes then.
+
+-- Conversion is an intermediary object, or perhaps just an intermediary type that never gets instantiated, which through some type magic (GADT specifying that it has to be of the Convertible typeclass), facilitates *: between only certain things.
+-- Conversion Unit Unit Unit - unit, unitFrom, unitTo, 
+
+-- (:*) Gets implemented with something like this:
+-- (:*) :: (UnitValue ut1) -> (UnitValue ut2) -> (UnitValue ut3)
+-- (:*) (UnitValue u1 v1) (UnitType u2 v2) = (UnitType u3 (v1 * v2)) where
+--     conversion :: Conversion u1 u2 u3 -- don't do anything with this, just make an error if this is invalid
+
+-- And instead of Cycle Amplitude, it'll be something like BaseAmplitude
+   
+
 -- The nature of units
 class Unit u where
     __ :: Float -> u
@@ -23,8 +43,8 @@ data Conversion domain perDomain where
 instance (Domain domain) => Unit (Progression domain) where
     __ n = Progression n
 
---instance (Range range) => Unit (Range range) where
---    __ n = Progression n
+instance (Range range) => Unit (!!! Hello range) where
+    __ n = Progression n
 
 instance (Domain domain, Domain perDomain) => Unit (Conversion domain perDomain) where
     __ n = Conversion n
