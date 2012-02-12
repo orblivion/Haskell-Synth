cf71fd7b985bebc82c1bc475a6eed558b0d65141
Daniel Krol <orblivion@gmail.com>
2012-02-12 09:41:48 -0600

    Adds a couple more basic units, clears out a bunch of comments.

--- units.hs
+++ units.hs
@@ -4,14 +4,6 @@
     FunctionalDependencies
     #-}
 
-
--- Here's the overall plan:
-
--- Similarly, SignalType can specify what can come out of a component. Be it a frequency, or whatnot. Or maybe we don't want to play favorites with what can go into what... hmm. Maybe for InputTypes then.
-
--- And instead of Cycle Amplitude, it'll be something like BaseAmplitude
--- "Range", or "Value", I suppose, typeclass. For amplitude n stuff. But only maybe, just something to consider.
-
 -- The nature of units
 class Unit u where
     __dummy :: u -> u 
@@ -26,36 +18,46 @@ data UnitValue unit_type where
 
 -- The nature of sorts of units
 
--- Starting off with (+:), but We can have various functions over time that operate on Progressions,
--- since they'd be used as a marker of how far along a list of items (samples, etc).
-class (Unit u) => Progression u where
-    __dummy2 :: u -> u
-
+-- Order of operations
 infixr 6 +:
 infixr 7 *:
 infixr 7 /:
+
+-- Starting off with Progressions, with (+:), but we can have various functions over time that operate on Progressions,
+-- since they'd be used as a marker of how far along a list of items (samples, etc).
+class (Unit u) => Progression u where
+    __dummy2 :: u -> u
 
 -- Not implemented as a typeclass function because it works on Units the same way
 (+:) :: (Progression unit_type) => UnitValue unit_type -> UnitValue unit_type -> UnitValue unit_type
 (+:) (UnitValue a) (UnitValue b) = UnitValue (a + b)
 
+-- Next with how units interoperate.
 class (Unit top, Unit bottom, Unit result) => UnitRelationship top bottom result | top bottom -> result where
     (*:) :: UnitValue bottom -> UnitValue result -> UnitValue top
     (*:) (UnitValue bottomval) (UnitValue resultval) = UnitValue (bottomval * resultval)
     (/:) :: UnitValue top -> UnitValue bottom -> UnitValue result 
     (/:) (UnitValue topval) (UnitValue bottomval) = UnitValue (topval / bottomval)
 
--- I was hoping this would work, doesn't seem to.
+
+-- I was hoping the following would work, doesn't seem to:
 -- Requires UndecidableInstances and OverlappingInstances, in case I try to pick it up again. Any ideas out there?
 -- Basically I'm trying to implement the commutative property of multiplication, and a=b/c -> c=b/a
 --instance (UnitRelationship (UnitValue top) (UnitValue bottom) (UnitValue result)) => UnitRelationship (UnitValue top) (UnitValue result) (UnitValue bottom)
 
+-- Actual unit types and their interactions:
+
 data Hertz = Hertz 
 data Cycle = Cycle
 data Second = Second
+data Sample = Sample
 data Amplitude = Amplitude
 data SignalValue = SignalValue
+data SignalSlope = SignalSlope
+data SamplePerSecond = SamplePerSecond
 
+-- Some shortcuts, since the type of __ aka UnitValue is ambiguous, and Hertz etc are only a parameter.
+-- Would be nice if these were automatically made
 _second      = __ :: SafeValue -> UnitValue Second
 _cycle       = __ :: SafeValue -> UnitValue Cycle 
 _hertz       = __ :: SafeValue -> UnitValue Hertz
@@ -63,22 +65,29 @@ _amplitude   = __ :: SafeValue -> UnitValue Amplitude
 _signalvalue = __ :: SafeValue -> UnitValue SignalValue
 
 instance Unit Hertz
-instance Unit Cycle     -- Not a unit in the sense of physics. One cycle represents
+instance Unit SamplePerSecond
+instance Unit Sample
+instance Unit Cycle     -- Not a unit in the sense of physics. One cycle represents start to finish of a sine wave in a timeless domain
 instance Unit Second
-instance Unit Amplitude -- Not a unit in the sense of physics
-instance Unit SignalValue -- Definitely not a unit in the sense of physics
+instance Unit Amplitude -- Not a unit in the sense of physics. One amplitude represents the ability to transform a SignalValue
+instance Unit SignalValue -- Definitely not a unit in the sense of physics. We (explicitly) break unit laws by converting to other units.
+instance Unit SignalSlope
 
 instance Progression Cycle
 instance Progression Second
+instance Progression Sample
+
 
 instance UnitRelationship Cycle Second Hertz
-instance UnitRelationship Cycle Hertz Second -- lame that I have to do this manually. See "hoping this would work" above.
-instance UnitRelationship Amplitude SignalValue SignalValue -- This will make sure Amplitude inputs are used correctly.
+instance UnitRelationship Cycle Hertz Second -- lame that I have to do the commutative manually. See "hoping this would work" above.
 
--- This is a signal type. It can convert to errethang. But only explicitly!
--- Change this to SignalValue
---fromGeneric :: UnitValue Generic -> UnitValue 
---fromGeneric g val = __ 
+instance UnitRelationship Sample Second SamplePerSecond
+instance UnitRelationship Sample SamplePerSecond Second
+
+instance UnitRelationship SignalValue Sample SignalSlope
+instance UnitRelationship SignalValue SignalSlope Sample
+
+instance UnitRelationship Amplitude SignalValue SignalValue -- This will make sure Amplitude inputs are used correctly.
 
 get_frequency :: UnitValue Second -> UnitValue Cycle -> UnitValue Hertz
 get_frequency s c = c /: s 
@@ -87,94 +96,3 @@ get_seconds :: UnitValue Hertz -> UnitValue Cycle -> UnitValue Second
 get_seconds h c = c /: h
 
 add_5_seconds s = (s :: (UnitValue Second)) +: _second 5
-
-{-
-------------
-
-data Conversion domain perDomain where
-    Conversion :: (Domain domain, Domain perDomain) => Float -> Conversion domain perDomain
-
-instance (Domain domain) => Unit (Progression domain) where
-    __ n = Progression n
-
-instance (Range range) => Unit (!!! Hello range) where
-    __ n = Progression n
-
-instance (Domain domain, Domain perDomain) => Unit (Conversion domain perDomain) where
-    __ n = Conversion n
-
-
-
-
--- Things that domains, ranges, and conversions can do
-
-(+:) :: Progression domain -> Progression domain -> Progression domain
-(+:) (Progression a) (Progression b) = Progression (a + b)
-
-
--- It has been pointed out to me that this, as it stands, offers me no type safety.
-(*:) :: Progression domain_a -> Conversion domain_b domain_a -> Progression domain_b
-(*:) (Progression a) (Conversion f) = Progression (a * f)
-
-
-
-
--- Some specific domains and ranges
-
-data SecondDomain   = SecondDomain
-instance Domain SecondDomain
-type Second = Progression SecondDomain
-second :: Float -> Second
-second a = Progression a
-
-data CycleDomain    = CycleDomain
-instance Domain CycleDomain
-type Cycle = Progression CycleDomain
-cycle :: Float -> Cycle 
-cycle a = Progression a
-
-data SampleDomain   = SampleDomain
-instance Domain SampleDomain
-type Sample = Progression SampleDomain
-sample :: Float -> Sample
-sample a = Progression a
-
-type Frequency = Conversion CycleDomain SecondDomain
-frequency :: Float -> Frequency
-frequency a = Conversion a
-
-type SamplingRate = Conversion SampleDomain SecondDomain
-samplingrate :: Float -> SamplingRate 
-samplingrate a = Conversion a
-
-
---data AmplitudeRange = AmplitudeRange
---instance Range Amplitude
-
-
-
-a :: Cycle
-a =  second 5 +: __ 5  *: __ 20
-
-
-
---data Progression    = Progression   deriving Aspect
---data Slope          = Slope         deriving Aspect
---data SamplingRate   = SamplingRate  deriving Aspect
---data Frequency      = Frequency     deriving Aspect
-
-
---SamplingRate
-
---Progression
-
-
-
-
--- TimeSamplingRate = TimeSamples/Time
--- Frequency = NumCycles/Time
--- Time * Frequency = NumCycles
--- Time * SamplingRate = Samples
--- CycleSamplingRate = CycleSamples/NumCycles
-
--}
